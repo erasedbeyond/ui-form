7303390788696a319d5570fc62b1ee17fa6b9571
Shivendra Porwal <shivendra.porwal_v@wingify.com>
2021-05-28 05:11:28 +0530

    design Completed

--- src/components/Main.js
+++ src/components/Main.js
@@ -47,7 +47,6 @@ class Main extends React.Component{
                 showEmailAlert:'visible'
             })
             submit=false;
-
         }else{
             let pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
             if(!pattern.test(this.state.email)){
@@ -67,7 +66,6 @@ class Main extends React.Component{
             submit=false;
 
         }else{
-            // let regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
             let pattern = new RegExp(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{8,32}/)
 
             if(!pattern.test(this.state.password)){
@@ -77,45 +75,58 @@ class Main extends React.Component{
                     })
                 }else{
                     this.setState({
-                        passwordAlert:"Password must have numeric values",
+                        passwordAlert:"Password must have a numeric values",
                     })
                 }
                 this.setState({
                     showPasswordAlert:'visible'
                 })
                 submit=false;
-
             } 
         }
-        if(!submit){
-            // alert('not submited')
-        }else{
-            alert('submitted')
+
+        if(submit){
+            alert("form SUBMITTED\n"
+                +"EMAIL: "+this.state.email+"\n"
+                +"DOB: "+this.state.date+this.state.month+this.state.year+"\n"
+                +(this.state.organisation && "ORGANISATION: "+this.state.organisation)
+            )
+            this.setState({
+                email:'',
+                date:'3',
+                month:'march',
+                year:'1995',
+                organisation:'',
+                password:'',
+            })
+            e.target.reset()
         }
     }
 
 
     render(){
 
-        console.log(this.state)
-
         return(
-            <div className="main">
+            <main className="main">
                 <form onSubmit={this.submit} noValidate>
+
                     <h2>Create an account</h2>
+
                     <section >
                         <h3 className='required'>Enter your email</h3>
                         <span className={'alert ' +this.state.showEmailAlert} >{this.state.emailAlert}</span>
                         <input  type='email' name='email' onChange={this.setValues}/>
                     </section>
+
                     <section className='d-o-b'>
                         <h3 className='optional'>Date of birth</h3>
                         <div>
+
                             <label>
                                 Date
                                 <input type='number' name='date' placeholder={this.state.date} onChange={this.setValues}  min="1" max="31"/>
-                                
                             </label>
+
                             <label>
                                 Month
                                 <select name='month'  value={this.state.month} onChange={this.setValues}>
@@ -124,6 +135,7 @@ class Main extends React.Component{
                                     </option>))}
                                 </select>
                             </label>
+
                             <label>
                                 Year
                                 <select name='year' value={this.state.year} onChange={this.setValues}>
@@ -132,33 +144,35 @@ class Main extends React.Component{
                                     </option>))}
                                 </select>
                             </label>
+
                         </div>
                     </section>
+
                     <section >
                         <h3 className='required'>Choose a strong password</h3>
                         <span className={'alert ' +this.state.showPasswordAlert}>{this.state.passwordAlert}</span>
                         <input  type='password' name='password' onChange={this.setValues}/>
                     </section>
+
                     <section>
                         <h3 className='optional'>Are you an agency or individual?</h3>
                        <div>
-                        <label>
+                            <label>
                                 <input type='radio' value='individual' name='organisation' onChange={this.setValues}/>
                                 Individual
                             </label>
+
                             <label>
                                 <input type='radio' value='agency' name='organisation' onChange={this.setValues}/>
                                 Agency
                             </label>
                        </div>
-
                     </section>
 
                     <input className='submit-button' type="submit" value="Submit" />
-                </form>
 
-                
-            </div>
+                </form>
+            </main>
         );
     }
 }
